58d119bc5a89f126ac17cb3ef529c1522e165a19
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-21 23:57:16 +0300

    pending completion

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+Cards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+message = `Thank you, ${Cards[0][i]} for the wonderful ${Cards[1]}`;
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+    return message;
+    }
+}
+writeCards(message);
+*/
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]} for the wonderful surprise gift!`)
+    }
+    return names;
+  }
+writeCards(names);
+
+function countDown(num) {
+  for (let i = 0; i < num.length; i++) {
+    console.log(num);
+  }
+  return num;
+}
